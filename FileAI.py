377120c35e0b811eb498e2ead804a773b9b8e3e2
ilya-capok <158813778+ilya-capok@users.noreply.github.com>
2026-06-13 14:35:20 +0300

    Update FileAI.py

--- FileAI.py
+++ FileAI.py
@@ -1,22 +1,19 @@
-Открыть в Google Colab
-!pip install ultralytics opendatasets -q
-
-import opendatasets as od
 from ultralytics import YOLO
-from google.colab import files
-
+import opendatasets as od
 od.download("https://www.kaggle.com/datasets/ultralytics/taco")
-
-with open('/content/taco/data.yaml', 'w') as f:
+with open('taco/data.yaml', 'w') as f:
     f.write("""
-path: /content/taco
+path: taco
 train: train/images
 val: valid/images
-nc: 60
-names: ['garbage']
+nc: 1
+names:
+  - garbage
 """)
-
 model = YOLO('yolov8n.pt')
-model.train(data='/content/taco/data.yaml', epochs=30, imgsz=640, batch=16)
-
-files.download('/content/garbage_model/taco_detector/weights/best.pt')
+model.train(
+    data='taco/data.yaml',
+    epochs=30,
+    imgsz=640,
+    batch=16
+)
